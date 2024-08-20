9b21eca627c42bf5e50f61d3a2a099c57c6f007a
Vitaly <vya367@gmail.com>
2024-08-20 12:22:35 +0300

    fix error

--- module_2/module_2_5.py
+++ module_2/module_2_5.py
@@ -2,7 +2,7 @@ def get_matrix(n, m, value):
     matrix = []
     for i in range(n):
         row = []
-        for i in range(m):
+        for j in range(m):
             row.append(value)
         matrix.append(row)
     return matrix
