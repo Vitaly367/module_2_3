46714f5bf7c4e962ac7c94f6f4a31999d8df96ba
Vitaly367 <vya367@gmail.com>
2024-10-14 21:34:57 +0300

    Construction history

---
+++ module_5/module_5_4.py
@@ -0,0 +1,76 @@
+
+
+class House:
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        instance = super().__new__(cls)
+        if args and len(args) > 0:
+            house_name = args[0]
+            cls.houses_history.append(house_name)
+        return instance
+
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f"Название: {self.name}, количество этажей: {self.number_of_floors}"
+
+    def __eq__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+        return False
+
+    def __lt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+        return NotImplemented
+
+    def __le__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors <= other.number_of_floors
+        return NotImplemented
+
+    def __ne__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors != other.number_of_floors
+        return True
+
+    def __add__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors += value
+            return self
+        return NotImplemented
+
+    def __radd__(self, value):
+        return self.__add__(value)
+
+    def __iadd__(self, value):
+        return self.__add__(value)
+
+    def __del__(self):
+        print(f"{self.name} снесён, но он останется в истории")
+
+    def go_to(self, new_floor):
+        if 1 <= new_floor <= self.number_of_floors:
+            print(f"Перемещаемся на этаж {new_floor} в {self.name}")
+        else:
+            print("Ошибка: такого этажа не существует.")
+
+    # Пример выполнения программы:
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+
+# Удаление объектов
+del h2
+del h3
+
+print(House.houses_history)
